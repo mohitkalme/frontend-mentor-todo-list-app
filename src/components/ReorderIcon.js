057d6a6e4e65a6a71e39295bab8bd9a0f3afc800
Mohit kalme <68311332+mohitkalme@users.noreply.github.com>
2022-07-27 14:51:04 +0530

    adding onPan instead of onMouseDown

--- src/components/ReorderIcon.js
+++ src/components/ReorderIcon.js
@@ -3,7 +3,7 @@ import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 
 export function ReorderIcon({ dragControls }) {
   return (
-    <div className="dragIndicator" onMouseDown={(event) => dragControls.start(event)}>
+    <div className="dragIndicator" onPan={(event) => dragControls.start(event)} onPointerDown={(event) => dragControls.start(event)}>
       <DragIndicatorIcon  />
     </div>
   );
